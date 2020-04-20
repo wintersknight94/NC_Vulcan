f8e7229df9001f0d88ad938c41453664137c4aad
wintersknight94 <62315647+wintersknight94@users.noreply.github.com>
2020-04-20 15:12:27 -0500

    Add files via upload

---
+++ init.lua
@@ -0,0 +1,17 @@
+-- LUALOCALS < ---------------------------------------------------------
+local include, nodecore
+    = include, nodecore
+-- LUALOCALS > ---------------------------------------------------------
+
+
+minetest.register_alias_force("mapgen_lava_source", "air")
+
+include("vulcan")
+
+include("voxelarea_iterator")
+
+include("magma_veins")
+
+
+
+
