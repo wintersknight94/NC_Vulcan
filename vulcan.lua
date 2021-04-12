675c66f4007350b37be0ee8187d54a16989da9df
wintersknight94 <62315647+wintersknight94@users.noreply.github.com>
2021-04-11 21:38:17 -0500

    Increased vulcanization >:D>

--- vulcan.lua
+++ vulcan.lua
@@ -3,17 +3,18 @@ local minetest, nodecore
     = minetest, nodecore
 -- LUALOCALS > ---------------------------------------------------------
 local modname = minetest.get_current_modname()
-local function findlava(pos)
-	return nodecore.find_nodes_around(pos, "group:lava")
-end
+
+--local function findlava(pos)
+--	return nodecore.find_nodes_around(pos, "group:lava")
+--end
 
 --Vulcanize Soil--
 nodecore.register_abm({
 		label = "Vulcanize Soil",
 		nodenames = {"group:soil"},
 		neighbors = {"nc_terrain:lava_source", "nc_terrain:lava_flowing"},
-		interval = 4,
-		chance = 2,
+		interval = 10,
+		chance = 1,
 		action = function(pos)
 			nodecore.set_loud(pos, {name = "nc_concrete:adobe"})
 		end
@@ -31,4 +32,27 @@ nodecore.register_abm({
 		end
 	})
 
+--Vulcanize Gravel--
+nodecore.register_abm({
+		label = "Vulcanize Gravel",
+		nodenames = {"nc_terrain:gravel"},
+		neighbors = {"nc_terrain:lava_source", "nc_terrain:lava_flowing"},
+		interval = 10,
+		chance = 1,
+		action = function(pos)
+			nodecore.set_loud(pos, {name = "nc_terrain:stone"})
+		end
+	})
+	
+--Vulcanize Carbon--
+nodecore.register_abm({
+		label = "Vulcanize Carbon",
+		nodenames = {"nc_tree:tree","nc_tree:root","nc_fire:coal8","group:mossy"},
+		neighbors = {"nc_terrain:lava_source", "nc_terrain:lava_flowing"},
+		interval = 10,
+		chance = 1,
+		action = function(pos)
+			nodecore.set_loud(pos, {name = "nc_concrete:coalstone"})
+		end
+	})
 
